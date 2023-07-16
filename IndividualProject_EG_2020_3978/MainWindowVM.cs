e17721cdc9b8a885a10d1545352a67199d59b6ac
KalangiJayakody <thathsarakalangi@gmail.com>
2023-07-16 12:40:36 +0530

    Updated version

---
+++ IndividualProject_EG_2020_3978/MainWindowVM.cs
@@ -0,0 +1,109 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01
+{
+    public  partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public  ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selectedUser=null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+
+
+        [RelayCommand]
+        public void messeag()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA value must be between 0 and 4.", "Error");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"{name} is Deleted successfuly.", "DELETED \a ");
+                
+            }
+            else
+            {
+                MessageBox.Show("Plese Select Student before Delete.", "Error");
+
+
+            }
+        }
+        [RelayCommand]
+        public void ExecuteEditStudentCommand()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student", "Warning!");
+            }
+        }
+
+        public  MainWindowVM()
+        {
+            users = new ObservableCollection<User>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/1.png", UriKind.Relative));
+            users.Add(new User(1,23, "Ahinsa", "Dewmini", "12/1/2000",image1,"thata@gmail.com", "0711732093"));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/2.png", UriKind.Relative));
+            users.Add(new User(2,22, "Senuk", "Shamin", "23/12/2001",image2,"fjosjf@gmail.com", "0711732042"));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new User(3,24, "Adithya", "Subash", "1/9/1999",image3,"fjoejfs@gmail.com", "0711733223"));
+            BitmapImage image4= new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new User(4,24, "Shihan", "Rowel", "12/09/1999", image4,"wojoaw@gmail.com", "0712322323"));
+
+        }
+    }
+}
---
+++ IndividualProject_EG_2020_3978/Model/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public BitmapImage Image { get; set; }  
+
+        public string  DateOfBirth{ get; set; }
+        public Double GPA { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+         
+        
+
+        public String ImagePath
+        {
+            get { return $"/Model/Images/{Image}"; }
+        }
+
+        public string AcademicAdvisor { get; set; }
+
+        public User(int id,int age, string firstName, string lastName, string dateOfBirth,BitmapImage  image,string email, string phoneNumber )
+        {
+            Id = id;
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Image = image;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        public User()
+        {
+        }
+    }
+}
